1d6b2f52f893078a24611b4008c9594e4fe8978a
Coty DiSalle <disa.coty@gmail.com>
2015-09-21 01:48:21 +0000

    Added defatul to email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,12 @@
 class ContactMailer < ActionMailer::Base
+  default to: 'disa.coty@gmail.com'
+  
   def contact_email(email,name,body)
     @name = name
     @email = email
     @body = body
-    
+
     mail(from: email, subject: "Contact form message")
-  
+
   end
-end  
+end
