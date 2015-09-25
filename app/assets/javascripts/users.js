5ae64cf414ec21d8488a83ecfab227bef8eab8ef
Coty DiSalle <disa.coty@gmail.com>
2015-09-25 07:19:32 +0000

    Added notice and styles

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-    if (!error) {gr
+
+    if (!error) {
       // Get the Stripe token:
       Stripe.createToken({
         number: ccNum,
@@ -20,14 +21,18 @@ $(document).ready(function() {
     }
     return false;
   }); // form submission
+  
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
     var f = $("#new_user");
+
     // Get the token from the response:
     var token = response.id;
+
     // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+
     // Submit the form:
-    f.get(0).submit();
+    f.get(0).submit(); 
   }
 });
