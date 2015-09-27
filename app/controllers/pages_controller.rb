a896dd804fd6fc9016a4da1b41d0c13b8777ae90
Coty DiSalle <disa.coty@gmail.com>
2015-09-27 20:45:39 +0000

    Added profiles form and complete action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @basic_plan = Plan.find(1)
     @pro_plan = Plan.find(2)
-    @variable = params[:hello]
+  
   end
   def about
   end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,20 @@ class ProfilesController < ApplicationController
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile
   end
+  def create
+    # Save values from form to profile
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
+  end
+
+  private
+    def profile_params
+     params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :descripti       on)
+    end
+
 end
