98b0c05d17d9273205832c82daf589fd7723fd57
Coty DiSalle <disa.coty@gmail.com>
2015-09-27 21:30:51 +0000

    Added barebones user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,11 +13,12 @@ class ProfilesController < ApplicationController
       redirect_to user_path( params[:user_id] )
     else
       render action: :new
+    end
   end
 
   private
     def profile_params
-     params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :descripti       on)
+     params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
 
 end
